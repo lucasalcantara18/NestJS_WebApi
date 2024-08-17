196378f3ee36ab9d4ba219c47d20d650e5c84aad
Lucas Alcântara <lucas.alcantara@finaxapp.com.br>
2024-08-17 17:38:48 -0300

    adding example of logger

--- src/application/useCases/distribute/distribute.service.ts
+++ src/application/useCases/distribute/distribute.service.ts
@@ -3,10 +3,12 @@ import { IDistributeService } from './idistribute.service';
 import { UserRepository } from 'src/infrastructure/dataAccess/repository/user.repository';
 import { TaskRepository } from 'src/infrastructure/dataAccess/repository/task.repository';
 import { User } from 'src/domain/models/user.entity';
+import { Logger } from '@nestjs/common';
 
 @Injectable()
 export class DistributeService implements IDistributeService {
     
+    readonly _logger = new Logger("OwnershipService");
     readonly _userRepository: UserRepository;
     readonly _taskRepository: TaskRepository;
 
@@ -16,6 +18,8 @@ export class DistributeService implements IDistributeService {
     }
 
     async findUserTask(idUser: string): Promise<User> {
+        this._logger.log(`finding tasks from user ${idUser}`);
+
         const user = await this._userRepository.findOneBy({id: idUser});
 
         if(!user)
